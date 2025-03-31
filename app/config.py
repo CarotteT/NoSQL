0bbcad0232aa6a12f22f38baccaf7ed3cd7714b0
PELLAN Emeline <pellan@et.esiea.fr>
2025-03-31 16:15:46 +0200

    removing fails

--- app/config.py
+++
@@ -1,7 +0,0 @@
-# Configuration pour la connexion à Neo4j
-NEO4J_URI = "neo4j+s://de28febf.databases.neo4j.io"  
-NEO4J_USER = "neo4j" 
-NEO4J_PASSWORD = "Sb60P66UHM6-aVu2rrVJxhdjuf_G3eYKYWvvdUsUv8I"  
-
-# Configuration pour la connexion à MongoDB
-MONGODB_URI = "mongodb+srv://caroline:2glCM03nB2k3PKzf@cluster0.auy7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
